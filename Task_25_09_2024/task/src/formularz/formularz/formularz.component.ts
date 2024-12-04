7301109f6a3dfd0aebe9f3984af2c203e5daf2c8
Jasiek121 <145430021+Jasiek121@users.noreply.github.com>
2024-12-04 09:21:32 +0100

    FormularzTask

---
+++ Task_25_09_2024/task/src/formularz/formularz/formularz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formularz',
+  standalone: true,
+  imports: [],
+  templateUrl: './formularz.component.html',
+  styleUrl: './formularz.component.css'
+})
+export class FormularzComponent {
+
+}
